0e9513606ddeba31acb7f94efac2a4bb8fd9ae32
poornimar07 <poornimarp2@gmail.com>
2026-04-01 19:25:12 +0530

    Added tasks and question-2 inside Dom mini projects

---
+++ Dom-mini-projects/tasks/question-2/script.js
@@ -0,0 +1,29 @@
+
+    // select h1 tag
+
+        var div=document.getElementById("result")
+       
+        // Create new element
+
+        function update()
+        {
+            var listitem=document.createElement("h1")
+            listitem.textContent="Hello"
+            div.append(listitem)
+        }
+
+
+
+        // var div=document.getElementById("result")
+
+        // function update() 
+        // {
+        //     // h1.textContent="Hello"
+
+        //     // h1.append("hello")
+
+        //     div.append("<h1>hello</h1>")
+
+        // }
+
+    
