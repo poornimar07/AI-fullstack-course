b68e2551db8e2caf2279421850125ac0e7dae488
poornimar07 <poornimarp2@gmail.com>
2026-04-03 18:55:31 +0530

    Added tasks and question-2 inside Dom mini projects

---
+++ Dom-mini-projects/tasks/question-3/script.js
@@ -0,0 +1,15 @@
+
+
+// Function to delete clicked item
+function deleteitem(event) {
+
+    // event.target means the element you clicked (h1 tag)
+    // remove() will delete that element from the page
+    event.target.remove();
+}
+
+
+
+
+
+
